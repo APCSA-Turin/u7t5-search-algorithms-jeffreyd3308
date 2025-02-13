ce64200980efc47c4bba988f66f583d9e41f26fa
jeffreyd3308 <jeffreyd3308@nycstudents.net>
2025-02-13 16:39:10 +0000

    linsearch&carrepair

--- src/main/java/com/example/project/Repair/RepairSchedule.java
+++ src/main/java/com/example/project/Repair/RepairSchedule.java
@@ -13,6 +13,8 @@ public class RepairSchedule {
      */
     public RepairSchedule(int n) {
         /* to be implemented in part (a) */
+        numberOfMechanics = n;
+        schedule = new ArrayList<CarRepair>();
     }
 
     public ArrayList<CarRepair> getSchedule() {
@@ -23,6 +25,15 @@ public class RepairSchedule {
      * Precondition: 0 <= m < numberOfMechanics and b >= 0
      */
     public boolean addRepair(int m, int b) {
+        for (int i = 0; i < schedule.size(); i++) {
+            if (schedule.get(i).getMechanicNum() == m) {
+                return false;
+            }
+            if (schedule.get(i).getBayNum() == b) {
+                return false;
+            }
+        }
+        schedule.add(new CarRepair(m, b));
         return true;
     }
 
@@ -30,7 +41,20 @@ public class RepairSchedule {
      * as described in part (b).
      */
     public ArrayList<Integer> availableMechanics() {
-        return new ArrayList<Integer>();
+        ArrayList<Integer> available = new ArrayList<Integer>();
+        for (int i = 0; i < numberOfMechanics; i++) {
+            boolean isIn = false;
+            for (int j = 0; j < schedule.size(); j++) {
+                if (i == schedule.get(j).getMechanicNum()) {
+                    isIn = true;
+                    break;
+                }
+            }
+            if (!isIn) {
+                available.add(i);
+            }
+        }
+        return available;
     }
 
     /** Removes an element from schedule when a repair is complete.
